7a5a1986953388e2eea5d0d5b049646a66e84695
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-10-09 16:10:11 +0800

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ParentWrapper from "./parent-wrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div>
           <ParentWrapper>{children}</ParentWrapper>
         </div>
+        <Analytics />
       </body>
     </html>
   );
